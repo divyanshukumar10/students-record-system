8bde42a6fdba95dd4794f3387e902a5d8a362d77
Divyanshu Kumar <38113387+divyanshukumar10@users.noreply.github.com>
2021-01-25 01:31:01 +0530

    Add files via upload

---
+++ searchStudent.cpp
@@ -0,0 +1,51 @@
+// searchStudent():
+// This function opens the binary file in reading mode and asks the user to enter the student id number which wants to search. If the student info is not available on the list, it shows the message student not find in records.
+void searchStudent()
+{
+int found = 0;
+int studentId =0;
+s_StudentInfo addStudentInfoInDataBase = {0};
+FILE *fp = NULL;
+fp = fopen(FILE_NAME,"rb");
+if(fp == NULL)
+{
+printf("\n\t\t\tFile is not opened\n");
+exit(1);
+}
+headMessage("SEARCH STUDENTS");
+//put the control on student detail
+if (fseek(fp,FILE_HEADER_SIZE,SEEK_SET) != 0)
+{
+fclose(fp);
+printf("\n\t\t\tFacing issue while reading file\n");
+exit(1);
+}
+printf("\n\n\t\t\tEnter Student ID NO to search:");
+fflush(stdin);
+scanf("%u",&studentId);
+while (fread (&addStudentInfoInDataBase, sizeof(addStudentInfoInDataBase), 1, fp))
+{
+if(addStudentInfoInDataBase.student_id == studentId)
+{
+found = 1;
+break;
+}
+}
+if(found)
+{
+printf("\n\t\t\tStudent id = %d\n",addStudentInfoInDataBase.student_id);
+printf("\n\t\t\tStudent name = %s",addStudentInfoInDataBase.studentName);
+printf("\t\t\tFather Name = %s",addStudentInfoInDataBase.fatherName);
+printf("\n\t\t\tStudent Address = %s",addStudentInfoInDataBase.studentAddr);
+printf("\t\t\tStudent Admission Date(day/month/year) = (%d/%d/%d)",addStudentInfoInDataBase.studentJoiningDate.dd,
+addStudentInfoInDataBase.studentJoiningDate.mm, addStudentInfoInDataBase.studentJoiningDate.yyyy);
+}
+else
+{
+printf("\n\t\t\tNo Record");
+}
+fclose(fp);
+printf("\n\n\n\t\t\tPress any key to go to main menu.....");
+fflush(stdin);
+getchar();
+}
