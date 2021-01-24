474e054cd64b326250a67f1173a09ef1bda7dc6d
Divyanshu Kumar <38113387+divyanshukumar10@users.noreply.github.com>
2021-01-25 01:31:17 +0530

    Add files via upload

---
+++ welcomeMessage.cpp
@@ -0,0 +1,18 @@
+// welcomeMessage():
+// This function displays the first welcomes screen of the “Student Record System Project in C” and asks the user to press any key to access the library application.
+void welcomeMessage()
+{
+headMessage("www.aticleworld.com");
+printf("\n\n\n\n\n");
+printf("\n\t\t\t **-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**\n");
+printf("\n\t\t\t =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+printf("\n\t\t\t = WELCOME =");
+printf("\n\t\t\t = TO =");
+printf("\n\t\t\t = Student Record =");
+printf("\n\t\t\t = MANAGEMENT =");
+printf("\n\t\t\t = SYSTEM =");
+printf("\n\t\t\t =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+printf("\n\t\t\t **-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**\n");
+printf("\n\n\n\t\t\t Enter any key to continue.....");
+getch();
+}
